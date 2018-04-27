75b6d55808f116488e586b99f68e3c7a803f19e6
Caipei-WANWEI <caipei.fan@wanweifund.com>
2018-04-27 14:40:54 +0800

    version init

---
+++ index.js
@@ -0,0 +1,38 @@
+/**
+ * @Author Caipei FAN(2272946937@qq.com)
+ * @CreateDate  2018/4/27
+ */
+class WebpackPrefixReplacePlugin{
+  // Default options
+  constructor (options = {}){
+    this.options = {
+      file_types: options.file_types ?  options.file_types : /.*?\.(html)$/,
+      prefix: options.prefix ? options.prefix : null,
+      replacement: options.replacement ? options.replacement : null
+    };
+  }
+
+  apply (compiler){
+    let options = this.options;
+
+    compiler.plugin('emit', function(compilation, callback){
+      for(var filename in compilation.assets){
+        if(options.file_types.test(filename)){
+          if(options.prefix != null && options.replacement != null){
+            let content = compilation.assets[filename].source().replace(options.prefix,options.replacement);
+            let len = content.length;
+            compilation.assets[filename]={
+              source: function(){
+                return content;
+              },
+              size: function(){
+                return len;
+              }
+            }
+          }
+        }
+      }
+      callback();
+    });
+  };
+}
